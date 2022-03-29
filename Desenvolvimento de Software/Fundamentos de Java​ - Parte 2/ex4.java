ede7596c210c2a30b57dfcb18a8842f46eb59630
witchwarlock666 <witchwarlock666@gmail.com>
2022-03-29 14:10:30 -0300

    Update ex4

--- Desenvolvimento de Software/Fundamentos de Java​ - Parte 2/ex4.java
+++ Desenvolvimento de Software/Fundamentos de Java​ - Parte 2/ex4.java
@@ -1,45 +1,48 @@
+// Fabricio Bertoncello Filho e Felipe Lacerda
+
 import java.util.Scanner;
 
+public class Ex4 {
 
+    public static void main(String args[]) {
+        Scanner catador = new Scanner(System.in);
 
-public class ex4 {
+        int x;
+        int i, j;
+        int y, yp;
+        y = 0;
+        yp = 0;
+        int[] a = new int[100];
 
- public static void main(String args[]) {
-    Scanner catador = new Scanner(System.in);
-    
-    int x;
-    int i, j;
-    int resulty, resultya;
-    resulty = 0;
-    resultya = 0;
-    int[] a = new int[100];
-    
-    System.out.println("Digite o x: ");
-    x = catador.nextInt();
-    System.out.println("Digite o i: ");
-    j = catador.nextInt();
+        System.out.println("Digite o valor de x: ");
+        x = catador.nextInt();
 
-    if(  j <=0 || x <= 0  ){
-        System.out.println("Valor inválido");
-    }
-    
-    else{
-        for(i = j;i>0; i--){
-            System.out.printf("Digite o A de posição %d: ", a);
-            a[i] = catador.nextInt(); 
-           
+        System.out.println("Digite o vlor de n: ");
+        j = catador.nextInt();
+
+        if (j <= 0 || x <= 0) {
+            System.out.println("Valor invalido");
         }
-        for(i = j; i>0 ; i--){
-        resulty += (int) a[i]*Math.pow(x, i);
-        System.out.printf(" Resultado da equação de Y (ausência de N): %d", resulty);
 
-        for(i = j; i>0 ; --i){
-        resultya += (int)i * a[i]*Math.pow(x, i);
-        System.out.printf(" Resultado da equação de Y (ausência de N): %d", resultya);
+        else {
+            for (i = 0; i < j; i++) {
+                System.out.printf("Digite o A de posiçao %d: ", i + 1);
+                a[i] = catador.nextInt();
+
+            }
+
+            i = j;
+            while (i > 0) {
+                y += a[i] * Math.pow(x, i--);
+            }
+            System.out.printf("y = %d\n", y);
+
+            i = j;
+            while (i > 0) {
+                yp += i * a[i] * Math.pow(x, --i);
+            }
+            System.out.printf("y'= %d", yp);
         }
-        
+        catador.close();
     }
 }
-}
-}
-
