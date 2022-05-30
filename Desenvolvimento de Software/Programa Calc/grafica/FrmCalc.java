ad792ebdce38b5d408b059da139e81a99b2ef43e
witchwarlock666 <witchwarlock666@gmail.com>
2022-05-30 18:23:05 -0300

    Terminei a calculadora pq tava com preguiça de terminar o jogo da memoria

--- Desenvolvimento de Software/Programa Calc/grafica/FrmCalc.java
+++ Desenvolvimento de Software/Programa Calc/grafica/FrmCalc.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import logica.Calculadora;
 // import logica.Calculadora.Operacao;
+import logica.Calculadora.Operacao;
 
 import javax.swing.*;
 //import logica.Calculadora.Operacao;
@@ -16,19 +17,19 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 
-
 public class FrmCalc extends JFrame {
     private JTextField jTextFieldx;
     private JTextField jTextFieldy;
     private Calculadora calculadora;
-    //private JOptionPane DropDown;
+    // private JOptionPane DropDown;
     private double cataValorFInal;
+
     public FrmCalc() {
-       
+
         this.calculadora = new Calculadora();
 
         setLayout(new BorderLayout());
-        //criando o Jpanel
+        // criando o Jpanel
         JPanel pnl = new JPanel(new GridLayout(3, 2, 5, 5));
         this.add(pnl);
         // criando os jtextfields --
@@ -38,147 +39,36 @@ public class FrmCalc extends JFrame {
         this.jTextFieldy = new JTextField();
         pnl.add(this.jTextFieldy);
         this.jTextFieldy.setEditable(true);
-     
 
-       
-        // // JPanel pnlCentro = new JPanel(new GridLayout(5, 4, 5, 5));
-        // // this.add(pnlCentro, BorderLayout.CENTER);
-
-        // JButton btn = new JButton("C");
-       
-
-        // btn = new JButton("*");
-        // btn.addActionListener(new AbstractAction() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {                
-        //         String txtAntigo = txtDisplay.getText();
-        //         calculadora.setOperando1(Double.parseDouble(txtAntigo.replace(",", ".")));
-        //         calculadora.setOperacao(Operacao.MULTIPLICACAO);
-        //         calculadora.setAguardando(true);
-        //         txtDisplay.setText("");
-        //     }
-        // });            
-        // // pnlCentro.add(btn);
-
-        // btn = new JButton("<html>a<sup>b</sup></html>");
-        // btn.addActionListener(new AbstractAction() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {                
-        //         String txtAntigo = txtDisplay.getText();
-        //         calculadora.setOperando1(Double.parseDouble(txtAntigo.replace(",", ".")));
-        //         calculadora.setOperacao(Operacao.POTENCIA);
-        //         calculadora.setAguardando(true);
-        //         txtDisplay.setText("");
-        //     }
-        // });            
-        // // pnlCentro.add(btn);
-
-       
-
-       
-        // btn = new JButton("/");
-        // btn.addActionListener(new AbstractAction() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {                
-        //         String txtAntigo = txtDisplay.getText();
-        //         calculadora.setOperando1(Double.parseDouble(txtAntigo.replace(",", ".")));
-        //         calculadora.setOperacao(Operacao.DIVISAO);
-        //         calculadora.setAguardando(true);
-        //         txtDisplay.setText("");
-        //     }
-        // });            
-        // // pnlCentro.add(btn);
-
-
-        // btn = new JButton("+");
-        // btn.addActionListener(new AbstractAction() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {                
-        //         String txtAntigo = txtDisplay.getText();
-        //         calculadora.setOperando1(Double.parseDouble(txtAntigo.replace(",", ".")));
-        //         calculadora.setOperacao(Operacao.SOMA);
-        //         calculadora.setAguardando(true);
-        //         txtDisplay.setText("");
-        //     }
-        // });   
-        // // pnlCentro.add(btn);
-
-
-        // btn = new JButton("-");
-        // btn.addActionListener(new AbstractAction() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {                
-        //         String txtAntigo = txtDisplay.getText();
-        //         calculadora.setOperando1(Double.parseDouble(txtAntigo.replace(",", ".")));
-        //         calculadora.setOperacao(Operacao.SUBTRACAO);
-        //         calculadora.setAguardando(true);
-        //         txtDisplay.setText("");
-        //     }
-        // });            
-        // // pnlCentro.add(btn);
-
-        // btn = new JButton("+/-");
-        // btn.addActionListener(new AbstractAction() {
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {
-        //         String txtAntigo = txtDisplay.getText();
-        //         double num = Double.parseDouble(txtAntigo.replace(",", "."));
-        //         num *= -1; // num = -1 * num
-        //         txtDisplay.setText(String.format("%f", num));
-        //     }
-        // });
-        // // pnlCentro.add(btn);
-
-        String operacao[]={"Multiplicação (*)", "Divisão (/)", "Soma (+)", "Subtração (-)", "Potenciação(a^b)"};        
+        String operacao[] = { "Soma (+)", "Subtração (-)", "Multiplicação (*)", "Divisão (/)", "Potenciação(a^b)" };
         JComboBox cb = new JComboBox(operacao);
         pnl.add(cb);
 
-       JButton btn = new JButton("=");
+        JButton btn = new JButton("=");
         btn.addActionListener(new AbstractAction() {
             @Override
-            public void actionPerformed(ActionEvent e) { 
-                if(calculadora.isAguardando()){
-                    String txtAntigo = jTextFieldx.getText();
-                    calculadora.setOperando1(Double.parseDouble(txtAntigo.replace(",", ".")));
-                    String txtAntigo2 = jTextFieldy.getText();
-                    calculadora.setOperando2(Double.parseDouble(txtAntigo.replace(",", ".")));
-                    calculadora.calcular(p1, p2) cb.getItemAt(cb.getSelectedIndex());
-                    cataValorFInal = calculadora.calcular(Double.parseDouble(txtAntigo), Double.parseDouble(txtAntigo2));
-                    
-                }  
-                      
-              
-                    
+            public void actionPerformed(ActionEvent e) {
+                String txtAntigo = jTextFieldx.getText();
+                calculadora.setOperando1(Double.parseDouble(txtAntigo.replace(",", ".")));
+                String txtAntigo2 = jTextFieldy.getText();
+                calculadora.setOperando2(Double.parseDouble(txtAntigo.replace(",", ".")));
+                calculadora.setOperacao(cb.getSelectedIndex());
+                cataValorFInal = calculadora.calcular();
+                Object[] options = { "SIM", "NÃO" };
+                JOptionPane.showOptionDialog(null,
+                        "Sua conta solicitada deu: " + cataValorFInal + "\n Deseja Continuar?",
+                        "Aviso",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                        null, options, options[0]);
+
             }
         });
         pnl.add(btn);
 
-    
-        
-        // Arrumar o retorno de conta, é só colocar dentro do action listener pra linkar o JComboBox com o método da outra clase("Calcular")
-            
-
-
-
-
-
-        if(calculadora.isAguardando()){
-            Object[] options = { "SIM", "NÃO"};
-        JOptionPane.showOptionDialog(null, "Sua conta solicitada deu: " + cataValorFInal + "\n Deseja Continuar?", "Aviso",
-          JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
-              null, options, options[0]);
-              calculadora.setAguardando(false);
-            }
-            
-        
         this.pack();
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-        }  
-        
-       
-     
-          
-           
+
+    }
+
 }
--- Desenvolvimento de Software/Programa Calc/logica/Calculadora.java
+++ Desenvolvimento de Software/Programa Calc/logica/Calculadora.java
@@ -2,17 +2,16 @@ package logica;
 
 public class Calculadora {
 
-     public ENUM Operacao {
-            SOMA,
-            SUBTRACAO,
-            MULTIPLICACAO,
-            DIVISAO,
-            POTENCIA;
-     }
+    public enum Operacao {
+        SOMA,
+        SUBTRACAO,
+        MULTIPLICACAO,
+        DIVISAO,
+        POTENCIA;
+    };
 
     private double operando1;
     private double operando2;
-    private boolean aguardando;
     private Operacao operacao;
 
     public double getOperando1() {
@@ -23,12 +22,8 @@ public class Calculadora {
         return operando2;
     }
 
-    public boolean isAguardando() {
-        return aguardando;
-    }
-
     public Operacao getOperacao() {
-       return operacao;
+        return operacao;
     }
 
     public void setOperando1(double operando1) {
@@ -38,23 +33,28 @@ public class Calculadora {
     public void setOperando2(double operando2) {
         this.operando2 = operando2;
     }
-    public void setAguardando(boolean aguardando) {
-        this.aguardando = aguardando;
-    }
-    
-     public void setOperacao(Operacao operacao) {
-         this.operacao = operacao;
-     }
 
-    public double calcular(Double p1, Double p2, int operacao) {
+    public void setOperacao(Operacao operacao) {
+        this.operacao = operacao;
+    }
+
+    public void setOperacao(int op) {
+        operacao = Operacao.values()[op];
+    }
+
+    public double calcular() {
 
         switch (operacao) {
-            case 0: return p1 + p2;
-            case 1: return p1 - p2;
-            case 2: return p1 * p2;
-            case 3: return p1 / p2;
-            default: return Math.pow(p1, p2);
+            case SOMA:
+                return operando1 + operando2;
+            case SUBTRACAO:
+                return operando1 - operando2;
+            case MULTIPLICACAO:
+                return operando1 * operando2;
+            case DIVISAO:
+                return operando1 / operando2;
+            default:
+                return Math.pow(operando1, operando2);
         }
     }
 }
-
