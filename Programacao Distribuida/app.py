d091624dfa021281b5d014a435b7125d9c1f358c
witchwarlock666 <87949744+witchwarlock666@users.noreply.github.com>
2023-06-10 18:18:29 -0300

    aaaaaa

---
+++ Programacao Distribuida/app.py
@@ -0,0 +1,262 @@
+from time import time
+from flask import Flask, request, redirect, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dataclasses import dataclass
+from datetime import date, datetime
+  
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.debug = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@dataclass
+class Cliente(db.Model):
+    id: int
+    nome: str
+    senha: int
+    qtdMoeda: int
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(20), unique=False, nullable=False)
+    senha = db.Column(db.String(20), unique=False, nullable=False)
+    qtdMoeda = db.Column(db.Integer, unique=False, nullable=False)
+    
+    def getDict(self):
+        return {"id": self.id, "nome": self.nome, "senha": self.senha, "qtdMoeda": self.qtdMoeda}
+
+class Seletor(db.Model):
+    id: int
+    nome: str
+    ip: str
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(20), unique=False, nullable=False)
+    ip = db.Column(db.String(15), unique=False, nullable=False)
+    
+    def getDict(self):
+        return {"id": self.id, "nome": self.nome, "senha": self.ip}
+    
+class Transacao(db.Model):
+    id: int
+    remetente: int
+    recebedor: int
+    valor: int
+    status: int
+    
+    id = db.Column(db.Integer, primary_key=True)
+    remetente = db.Column(db.Integer, unique=False, nullable=False)
+    recebedor = db.Column(db.Integer, unique=False, nullable=False)
+    valor = db.Column(db.Integer, unique=False, nullable=False)
+    horario = db.Column(db.DateTime, unique=False, nullable=False)
+    status = db.Column(db.Integer, unique=False, nullable=False)
+    
+    def getDict(self):
+        return {"id": self.id, "remetente": self.remetente, "recebedor": self.recebedor, "valor": self.valor, "status": self.status}
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route("/")
+def index():
+    return render_template('api.html')
+
+@app.route('/cliente', methods = ['GET'])
+def ListarCliente():
+    if(request.method == 'GET'):
+        clientes = Cliente.query.all()
+        o = {}
+        for i in clientes:
+            a = i.getDict()
+            o[a["id"]] = a
+        return jsonify(o)  
+
+@app.route('/cliente/<string:nome>/<string:senha>/<int:qtdMoeda>', methods = ['POST'])
+def InserirCliente(nome, senha, qtdMoeda):
+    if request.method=='POST' and nome != '' and senha != '' and qtdMoeda != '':
+        objeto = Cliente(nome=nome, senha=senha, qtdMoeda=qtdMoeda)
+        db.session.add(objeto)
+        db.session.commit()
+        o = objeto.getDict()
+        return jsonify(objeto)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/cliente/<int:id>', methods = ['GET'])
+def UmCliente(id):
+    if(request.method == 'GET'):
+        objeto = Cliente.query.get(id)
+        o = objeto.getDict()
+        return jsonify(objeto)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/cliente/<int:id>/<int:qtdMoeda>', methods=["POST"])
+def EditarCliente(id, qtdMoeda):
+    if request.method=='POST':
+        try:
+            varId = id
+            varqtdMoeda = qtdMoeda
+            cliente = Cliente.query.filter_by(id=id).first()
+            db.session.commit()
+            cliente.qtdMoeda = qtdMoeda
+            db.session.commit()
+            o = cliente.getDict()
+            return jsonify(o)
+        except Exception as e:
+            data={
+                "message": "Atualização não realizada"
+            }
+            return jsonify(data)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/cliente/<int:id>', methods = ['DELETE'])
+def ApagarCliente(id):
+    if(request.method == 'DELETE'):
+        objeto = Cliente.query.get(id)
+        db.session.delete(objeto)
+        db.session.commit()
+
+        data={
+            "message": "Cliente Deletado com Sucesso"
+        }
+
+        return jsonify(data)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/seletor', methods = ['GET'])
+def ListarSeletor():
+    if(request.method == 'GET'):
+        produtos = Seletor.query.all()
+        o = {}
+        for i in produtos:
+            a = i.getDict()
+            o[a["id"]] = a
+        return jsonify(o)
+
+@app.route('/seletor/<string:nome>/<string:ip>', methods = ['POST'])
+def InserirSeletor(nome, ip):
+    if request.method=='POST' and nome != '' and ip != '':
+        objeto = Seletor(nome=nome, ip=ip)
+        db.session.add(objeto)
+        db.session.commit()
+        o = objeto.getDict()
+        return jsonify(objeto)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/seletor/<int:id>', methods = ['GET'])
+def UmSeletor(id):
+    if(request.method == 'GET'):
+        produto = Seletor.query.get(id)
+        o = produto.getDict()
+        return jsonify(produto)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/seletor/<int:id>/<string:nome>/<string:ip>', methods=["POST"])
+def EditarSeletor(id, nome, ip):
+    if request.method=='POST':
+        try:
+            varNome = nome
+            varIp = ip
+            validador = Seletor.query.filter_by(id=id).first()
+            db.session.commit()
+            validador.nome = varNome
+            validador.ip = varIp
+            db.session.commit()
+            o = validador.getDict()
+            return jsonify(o)
+        except Exception as e:
+            data={
+                "message": "Atualização não realizada"
+            }
+            return jsonify(data)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/seletor/<int:id>', methods = ['DELETE'])
+def ApagarSeletor(id):
+    if(request.method == 'DELETE'):
+        objeto = Seletor.query.get(id)
+        db.session.delete(objeto)
+        db.session.commit()
+
+        data={
+            "message": "Validador Deletado com Sucesso"
+        }
+
+        return jsonify(data)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/hora', methods = ['GET'])
+def horario():
+    if(request.method == 'GET'):
+        objeto = datetime.now()
+        return jsonify(objeto)
+
+@app.route('/transacoes', methods = ['GET'])
+def ListarTransacoes():
+    if(request.method == 'GET'):
+        transacoes = Transacao.query.all()
+        o = {}
+        for i in transacoes:
+            a = i.getDict()
+            o[a["id"]] = a
+        return jsonify(o)
+    
+    
+@app.route('/transacoes/<int:rem>/<int:reb>/<int:valor>', methods = ['POST'])
+def CriaTransacao(rem, reb, valor):
+    if request.method=='POST':
+        objeto = Transacao(remetente=rem, recebedor=reb,valor=valor,status=0,horario=datetime.now())
+        print(objeto)
+        db.session.add(objeto)
+        db.session.commit()
+        o = objeto.getDict()
+        return jsonify(o)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/transacoes/<int:id>', methods = ['GET'])
+def UmaTransacao(id):
+    if(request.method == 'GET'):
+        objeto = Transacao.query.get(id)
+        o = objeto.getDict()
+        return jsonify(objeto)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.route('/transactions/<int:id>/<int:status>', methods=["POST"])
+def EditaTransacao(id, status):
+    if request.method=='POST':
+        try:
+            objeto = Transacao.query.filter_by(id=id).first()
+            db.session.commit()
+            objeto.id = id
+            objeto.status = status
+            db.session.commit()
+            o = objeto.getDict()
+            return jsonify(o)
+        except Exception as e:
+            data={
+                "message": "transação não atualizada"
+            }
+            return jsonify(data)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', debug=True, port=1234)
---
+++ Programacao Distribuida/validador/validador.py
@@ -0,0 +1,169 @@
+from flask import Flask, redirect, jsonify
+import sqlite3
+from datetime import datetime, timedelta
+import requests
+
+app = Flask(__name__)
+
+def getTime():
+    db = sqlite3.connect("validador.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    conn = db.cursor()
+    
+    url = conn.execute("select url from url where id = ?", (1,)).fetchone()
+    url = url[0] + "/hora"
+    r = requests.get(url = url)
+    time = r.json()
+    print(time)
+    return time
+
+@app.route("/seturl/<string:url>")
+def seturl(url):
+    db = sqlite3.connect("validador.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    conn = db.cursor()
+    
+    url = "http://" + url
+    
+    conn.execute("update url set url = ?", (url,))
+    db.commit()
+    return redirect("/")
+
+@app.route("/")
+def root():
+    return "aaa"
+
+@app.route("/createdb", methods = ['GET'])
+def createDatabase():
+    db = sqlite3.connect("validador.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    conn = db.cursor()
+    
+    conn.execute("""create table transactions(
+                    id integer not null primary key autoincrement,
+                    id_pay integer not null,
+                    id_get integer not null,
+                    value integer not null,
+                    amount integer not null,
+                    approved integer not null,
+                    time timestamp not null
+                )""")
+    
+    conn.execute("""create table users(
+                    id integer not null primary key autoincrement,
+                    id_user integer not null,
+                    blocked integer not null,
+                    time timestamp
+                )""")
+    
+    conn.execute("""create table url(
+                    id integer not null primary key autoincrement,
+                    url text
+                )""")
+    conn.execute("insert into url (url) values (?)", ("",))
+    db.commit()
+    
+    conn.close()
+    return redirect("/")
+
+@app.route("/teste", methods = ['GET'])
+def teste():
+    db = sqlite3.connect("validador.db")
+    conn = db.cursor()
+        
+    t = datetime.now()
+    tstr = datetime.strftime(t, "%a, %d %b %Y %H:%M:%S %Z")
+    diff = t - timedelta(seconds=1)
+    
+    for i in range(0, 1000):
+        conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (1, 2, 10, 10, 1, t))
+        db.commit()
+        select = conn.execute("select time from transactions where id_pay = ? and time between ? and ?", (1, diff, t)).fetchall()
+    
+        if len(select) >= 1000:
+            conn.execute("update users set blocked = 1, time = ? where id_user = ?", (t + timedelta(minutes=1), 1))
+            conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (1, 2, 10, 10, 0, t))
+            db.commit()
+    
+    userblock = conn.execute("select blocked, time from users where id_user = ?", (1,)).fetchone()
+        
+    if not userblock == None:
+        if userblock[0] == 1:
+            if tstr > userblock[1]:
+                conn.execute("update users set blocked = 0 where id_user = ?", (1,))
+                db.commit()
+            else:
+                conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (1, 2, 10, 10, 1, t))
+                db.commit()
+                return jsonify({"status": 2, "key": 123})
+    else:
+        conn.execute("insert into users (id_user, blocked) values (?, ?)", (1, 0))
+        db.commit()
+    
+    select = conn.execute("select time from transactions where id_pay = ? and time between ? and ?", (1, diff, t)).fetchall()
+    
+    if len(select) >= 1000:
+        conn.execute("update users set blocked = 1, time = ? where id_user = ?", (t + timedelta(minutes=1), 1))
+        conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (1, 2, 10, 10, 0, t))
+        db.commit()
+        return jsonify({"status": 2, "key": 123})
+    
+    if 10 <= 10:
+        conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (1, 2, 10, 10, 1, t))
+        db.commit()
+        
+        return jsonify({"status": 1, "key": 123})
+    
+    conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (1, 2, 10, 10, 0, t))
+    db.commit()
+    return jsonify({"status": 2, "key": 123})
+        
+    
+
+@app.route("/validar/<int:pay>/<int:get>/<int:value>/<int:amount>/<int:key>", methods = ['GET'])
+def validar(pay, get, value, amount, key):
+    try:
+        db = sqlite3.connect("validador.db")
+        conn = db.cursor()
+        
+        t = datetime.strptime(getTime(), "%a, %d %b %Y %H:%M:%S %Z")
+        tstr = datetime.strftime(t, "%a, %d %b %Y %H:%M:%S %Z")
+        
+        userblock = conn.execute("select blocked, time from users where id_user = ?", (pay,)).fetchone()
+        
+        if not userblock == None:
+            if userblock[0] == 1:
+                if tstr > userblock[1]:
+                    conn.execute("update users set blocked = 0 where id_user = ?", (pay,))
+                    db.commit()
+                else:
+                    conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (pay, get, value, amount, 1, t))
+                    db.commit()
+                    return jsonify({"status": 2, "key": key})
+        else:
+            conn.execute("insert into users (id_user, blocked) values (?, ?)", (pay, 0))
+            db.commit()
+        
+        diff = t - timedelta(seconds=1)
+        
+        select = conn.execute("select time from transactions where id_pay = ? and time between ? and ?", (pay, diff, t)).fetchall()
+        
+        if len(select) >= 1000:
+            conn.execute("update users set blocked = 1, time = ? where id_user = ?", (t + timedelta(minutes=1), pay))
+            conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (pay, get, value, amount, 0, t))
+            db.commit()
+            return jsonify({"status": 2, "key": key})
+        
+        if value <= amount:
+            conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (pay, get, value, amount, 1, t))
+            db.commit()
+            
+            return jsonify({"status": 1, "key": key})
+        
+        conn.execute("insert into transactions (id_pay, id_get, value, amount, approved, time) values (?, ?, ?, ?, ?, ?)", (pay, get, value, amount, 0, t))
+        db.commit()
+        return jsonify({"status": 2, "key": key})
+    
+    except Exception as e:
+        return jsonify({"status": 0, "key": key, "exception": str(e)})
+    
+
+if __name__ == "__main__":
+    app.run()
